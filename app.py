243cbd3d9f3c5c8bfc34ed7f840ffe9e201e1c74
hardikdhoundiyal24 <151952943+hardikdhoundiyal24@users.noreply.github.com>
2024-09-07 14:28:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,21 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     qr_code = None
-    entered_text = ""  # Initialize entered text as empty
+    entered_text = ""  
 
     if request.method == 'POST':
-        # Get the data from the form
         entered_text = request.form.get('data')
 
-        # Generate the QR code only if data is provided
+        
         if entered_text:
             img = qrcode.make(entered_text)
             buf = BytesIO()
             img.save(buf, format='PNG')
             buf.seek(0)
 
-            # Convert the image to base64 string
+            
             qr_code = base64.b64encode(buf.getvalue()).decode('utf-8')
-
-    # Pass the entered_text to the template to keep it in the input field
+ 
     return render_template('index.html', qr_code=qr_code, entered_text=entered_text)
 
 if __name__ == '__main__':
